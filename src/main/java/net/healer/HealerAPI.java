98d04fccc4fa94672648d28cf98b5ccc04fea58f
HealerPlusMC <healerplus908@gmail.com>
2025-01-02 18:32:57 +0200

    init comment

--- src/main/java/net/healer/HealerAPI.java
+++ src/main/java/net/healer/HealerAPI.java
@@ -7,13 +7,11 @@ import org.slf4j.LoggerFactory;
 
 public class HealerAPI implements ModInitializer {
 	public static final String MOD_ID = "healer_api";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
